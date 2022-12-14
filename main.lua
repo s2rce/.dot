a9a1a65c50d1b417904e8d62a704ceb8228d57f8
source <119382061+s2rce@users.noreply.github.com>
2022-12-13 21:44:29 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://github.com/s2rce/.dot/blob/2b9eb371d96a5b3c717991dfa037a9c2418cded5/loader.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/s2rce/.dot/2b9eb371d96a5b3c717991dfa037a9c2418cded5/loader.lua"))()
