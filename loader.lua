2b9eb371d96a5b3c717991dfa037a9c2418cded5
source <119382061+s2rce@users.noreply.github.com>
2022-12-13 21:41:21 -0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,12 @@
+local url = "https://raw.githubusercontent.com/s2rce/.dot/main/games"
 
+local games = {
+    [155615604] = "Prison Life";
+    [6407649031] = "No-Scope Arcade";
+}
+
+for i,v in next, games do
+    games[i] = table.concat(v:split(' '), '_')
+end
+
+return loadstring(game:HttpGet(url.. "/"..(games[game.PlaceId] or games[game.GameId] or "Universal")..".lua"))()
